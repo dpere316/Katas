6bfcad466b2cea4226f2a1a8ceed7430ddbbb5ce
Daniel Perz <dpere316@fiu.edu>
2020-08-10 13:13:27 -0400

    Kata 08-10

--- index.js
+++ index.js
@@ -16,4 +16,8 @@ function hello(name){
 
   console.log(hello('joHn'))
 
-  
+//   Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+  function greet(name){
+    return `Hello, ${name} how are you doing today?`
+  }
