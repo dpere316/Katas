e892cb0d0586275c3be26e0a47dfa0a13531fa5b
Daniel Perz <dpere316@fiu.edu>
2020-08-19 17:13:28 -0400

    Intersection of two arrays

--- Arrays.js
+++ Arrays.js
@@ -173,9 +173,15 @@ function birthdayCakeCandles(ar) {
             }
             return count
           }
-          
+
 // Given two arrays, write a function to compute their intersection.
           var intersection = function(nums1, nums2) {
             let arr = nums1.filter(value => nums2.includes(value))
             return [...new Set(arr)]
            };
+           
+//  Similar Solution
+           var intersection = function(nums1, nums2) {
+            return nums1.filter((ele) => nums2.includes(ele)) // First find the subset of num1 & num2 (with duplicate)
+            .filter((ele, index, arr) => arr.indexOf(ele) === index); // Then deduplicate the subset by only keeping the first element appears
+        };
