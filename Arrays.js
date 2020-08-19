c47bea250d2f1b4e0253fe54827181c443cf677a
Daniel Perz <dpere316@fiu.edu>
2020-08-19 15:55:24 -0400

    DivisbleSumPairs

--- Arrays.js
+++ Arrays.js
@@ -149,3 +149,27 @@ function birthdayCakeCandles(ar) {
             return []
         }
 
+
+// Complete the divisibleSumPairs function below.
+// n: the integer length of array 
+// k: the integer to divide the pair sum by
+// ar: an array of integers
+// return the number of pairs that are evenly divisble by K 
+
+        function divisibleSumPairs(n, k, ar) {
+
+          let count = 0;
+          
+            for(let i = 0; i < n; i++)
+            {
+              for(let j = i + 1; j < n; j++)
+              {
+                if((ar[i] + ar[j]) % k === 0)
+                {
+                 count++
+                }
+                
+              }
+            }
+            return count
+          }
