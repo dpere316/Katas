653f20260395df5c77867abba2a01aa300f2414f
Daniel Perz <dpere316@fiu.edu>
2020-09-27 17:39:50 -0400

    triple trouble

--- index.js
+++ index.js
@@ -173,3 +173,15 @@ function reject(array, predicate) {
 }, [])
 
 }
+
+function tripleTrouble(one, two, three){
+
+  let str = ""
+  
+  for(let i = 0; i < one.length; i++)
+  {
+    str += one[i] + two[i] + three[i]
+  }
+  
+  return str
+ }
