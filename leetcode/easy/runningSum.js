98a2e0162b94b7d6f81789be767e1cc3a8b74280
Daniel Perz <dpere316@fiu.edu>
2020-11-07 17:13:01 -0500

    Running 1d Array Sum Solution 2

--- leetcode/easy/runningSum.js
+++ leetcode/easy/runningSum.js
@@ -18,3 +18,20 @@ var runningSum = function(nums) {
 // Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
 
 // Return the running sum of nums.
+
+// Solution 2 using Array Map 
+
+var runningSum = function(nums) {
+    let sum = 0 
+  
+    let runningSum = nums.map((total) => {
+      sum += total
+      
+      return sum
+    })
+    return runningSum
+  };
+
+
+//   [1,2,3,4] =>   Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4] =>
+//   Ans: [ 1, 3, 6, 10 ]
