4b7c948300a78a7f81975b689545d2a06a1ba8eb
Daniel Perz <dpere316@fiu.edu>
2020-12-04 08:29:59 -0500

    Python

---
+++ codingBat/notString.py
@@ -0,0 +1,19 @@
+def not_string(str):
+      
+  if(not str.startswith('not')):
+    
+    return 'not ' + str
+    
+  else:
+    
+    return str
+
+
+# https://codingbat.com/prob/p189441
+
+# Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged.
+
+
+# not_string('candy') → 'not candy'
+# not_string('x') → 'not x'
+# not_string('not bad') → 'not bad'
