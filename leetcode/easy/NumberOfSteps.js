cad95461c3fd440be1182ed40467d1461e84856f
Daniel Perz <dpere316@fiu.edu>
2021-02-13 17:53:42 -0500

    1342. Number of Steps to Reduce a Number to Zero

---
+++ leetcode/easy/NumberOfSteps.js
@@ -0,0 +1,25 @@
+var numberOfSteps  = function(num) {
+
+    let countSteps = 0; 
+  
+      while(num !== 0)
+      {
+        if(num % 2 === 0)
+        {
+          num = num / 2
+          countSteps++
+        }
+        if(num % 2 !== 0)
+        {
+          num = num - 1
+          countSteps++
+        }
+      }
+      return countSteps
+  };
+  
+  console.log(numberOfSteps(123))
+  
+  // Given a non-negative integer num, return the number of steps to reduce it to zero. If the current number is even,you have to divide it by 2, otherwise, you have to subtract 1 from it.
+
+  
