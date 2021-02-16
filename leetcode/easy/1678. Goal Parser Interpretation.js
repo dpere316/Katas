5216b137dcd566909874b6597ec1763457876db0
Daniel Perz <dpere316@fiu.edu>
2021-02-16 16:26:23 -0500

    1678. Goal Parser Interpretation

--- leetcode/easy/1678. Goal Parser Interpretation.js
+++ leetcode/easy/1678. Goal Parser Interpretation.js
@@ -1,10 +1,21 @@
-var interpret = function(command) {
-    
-    let G = "G"
-    let O = "()"
-    let A = "(al)"
-    
-    
-  };
+var interpret = function (command) {
+  let G = /G/g;
+  let O = /\(\)/g;
+  let AL = /\(al\)/g;
+
+  command = command.replace(G, "G"); // G()()(al)
+  command = command.replace(O, "o"); // Goo(al)
+  command = command.replace(AL, "al"); // Gooal
+
+  return command;
+};
+
+console.log(interpret("G()()(al)"))
 
 // https://leetcode.com/problems/goal-parser-interpretation/
+
+// You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
+
+// Example:
+// Input: command = "G()()()()(al)"
+// Output: "Gooooal"
